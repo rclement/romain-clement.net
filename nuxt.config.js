91e60322337c8688d92a9f4426f279c7285ed323
Romain Clement <r.clement88@gmail.com>
2018-12-11 01:14:16 +0100

    Fix external font resource embedding

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,7 +30,8 @@ module.exports = {
       { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Quicksand:300,400' }
     ]
   },
 
