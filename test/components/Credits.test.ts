7102e56d685a5c2902881f859a70db73bec15506
Romain Clement <contact@romain-clement.net>
2020-04-11 12:18:48 +0200

    Refactor credits test

--- test/components/Credits.test.ts
+++ test/components/Credits.test.ts
@@ -1,35 +1,34 @@
 import Vue from 'vue'
-import { RouterLinkStub, Wrapper, createLocalVue, mount } from '@vue/test-utils'
+import { RouterLinkStub, createLocalVue, mount } from '@vue/test-utils'
 import Buefy from 'buefy'
 import Credits from '~/components/Credits.vue'
 
-const localVue = createLocalVue()
-localVue.use(Buefy)
+function createWrapper(component: Vue.VueConstructor<Vue>) {
+  const localVue = createLocalVue()
+  localVue.use(Buefy)
+
+  const mocks = {
+    $t: (msg: string) => msg,
+    $i18n: { locales: [] },
+    localePath: (path: string) => path,
+    switchLocalePath: (code: string) => code,
+  }
+
+  const stubs = {
+    NuxtLink: RouterLinkStub,
+    i18n: true,
+  }
+
+  return mount(component, {
+    localVue,
+    mocks,
+    stubs,
+  })
+}
 
 describe('components/credits', () => {
-  let wrapper: Wrapper<Vue>
-
-  beforeEach(() => {
-    wrapper = mount(Credits, {
-      localVue,
-      mocks: {
-        $t: (msg: string) => msg,
-        $i18n: { locales: [] },
-        localePath: (path: string) => path,
-        switchLocalePath: (code: string) => code,
-      },
-      stubs: {
-        NuxtLink: RouterLinkStub,
-        i18n: true,
-      },
-    })
-  })
-
-  afterEach(() => {
-    wrapper.destroy()
-  })
-
   it('is a Vue instance', () => {
+    const wrapper = createWrapper(Credits)
     expect(wrapper.isVueInstance()).toBeTruthy()
   })
 })
