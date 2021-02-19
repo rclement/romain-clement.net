4e35ee97e221f717fd632cf16f226b47b057fb3d
rclement <rclement@users.noreply.github.com>
2021-02-19 12:35:32 +0000

    deploy: f89e534b74f3320cfaad00beeb87bb32badb58d0

---
+++ _nuxt/static/1613738082/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1613738082",layout:"default",error:a,serverRendered:true,routePath:b,config:{app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8e59f6d2"}}}}(null,"\u002F"));
---
+++ _nuxt/static/1613738082/und/articles/cloudflare-setup-cdn-paas/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1613738082",layout:"default",error:a,serverRendered:true,routePath:"\u002Fund\u002Farticles\u002Fcloudflare-setup-cdn-paas",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8e59f6d2"}}}}(null));
