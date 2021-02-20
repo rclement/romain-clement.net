703015e28682e10b5dbc437bb32152859021a47c
rclement <rclement@users.noreply.github.com>
2021-02-20 15:16:06 +0000

    deploy: cc3e0a34a12312bfc8af808b21a37f4615f6fbe1

--- _nuxt/static/1613738082/reading/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1613738082",layout:"default",error:a,serverRendered:true,routePath:"\u002Freading",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8e59f6d2"}}}}(null));
