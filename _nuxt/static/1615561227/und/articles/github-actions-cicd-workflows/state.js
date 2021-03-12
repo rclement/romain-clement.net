b0d378e38f5e98e8d1c7ef94e55587c49230d632
rclement <rclement@users.noreply.github.com>
2021-03-12 15:02:08 +0000

    deploy: 517eadd08121e69b75579dc062585cc3eaebf298

---
+++ _nuxt/static/1615561227/und/articles/github-actions-cicd-workflows/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1615561227",layout:"default",error:a,serverRendered:true,routePath:"\u002Fund\u002Farticles\u002Fgithub-actions-cicd-workflows",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b4ec3b54"}}}}(null));
