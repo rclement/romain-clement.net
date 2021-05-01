dbd20a93194b72fafb1eec1c8d329d6c740aeb78
rclement <rclement@users.noreply.github.com>
2021-05-01 12:29:17 +0000

    deploy: 220d1345372d9a4be9fbf4080bea47d2587502ea

---
+++ _nuxt/static/1619872097/reading/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1619872097",layout:"default",error:a,serverRendered:true,routePath:"\u002Freading",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"3709f369"}}}}(null));
