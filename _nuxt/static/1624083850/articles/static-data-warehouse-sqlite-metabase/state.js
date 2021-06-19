4579e9da6b2405b1b5c81d975558e1d56d73f11e
rclement <rclement@users.noreply.github.com>
2021-06-19 06:25:08 +0000

    deploy: 2aaf982aa2b8f21ef77e804e0e662a09e0881ac1

---
+++ _nuxt/static/1624083850/articles/static-data-warehouse-sqlite-metabase/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624083850",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticles\u002Fstatic-data-warehouse-sqlite-metabase",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ccf6a77d"}}}}(null));
