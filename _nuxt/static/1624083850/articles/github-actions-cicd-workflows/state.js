5f6a65749e67ef211daf6e8c092ff1db357aa21e
rclement <rclement@users.noreply.github.com>
2021-06-19 12:48:34 +0000

    deploy: 598644a5f3ba2fad52c06489067c0bbd631c6d2f

--- _nuxt/static/1624083850/articles/github-actions-cicd-workflows/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624083850",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticles\u002Fgithub-actions-cicd-workflows",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ccf6a77d"}}}}(null));
---
+++ _nuxt/static/1624106866/articles/python-packages-development-pipenv/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624106866",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticles\u002Fpython-packages-development-pipenv",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"3ac25e73"}}}}(null));
