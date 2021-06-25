03806eb9297b054627059fd6e0e765694e6b73e2
rclement <rclement@users.noreply.github.com>
2021-06-25 08:19:43 +0000

    deploy: c850beeaed875feef3887dda2145e752715fb500

--- _nuxt/static/1624457246/und/articles/python-packages-development-pipenv/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624457246",layout:"default",error:a,serverRendered:true,routePath:"\u002Fund\u002Farticles\u002Fpython-packages-development-pipenv",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6a89fca1"}}}}(null));
--- _nuxt/static/1624457246/und/reading/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624457246",layout:"default",error:a,serverRendered:true,routePath:"\u002Fund\u002Freading",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6a89fca1"}}}}(null));
