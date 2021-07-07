e5da630ed82e3b160933ba4cb05bc74ca34ae050
rclement <rclement@users.noreply.github.com>
2021-07-07 10:03:39 +0000

    deploy: 86d8e7dae2fda1a533ba869a3e10942792c20bc5

--- _nuxt/static/1625649621/und/articles/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1625649621",layout:"default",error:a,serverRendered:true,routePath:"\u002Fund\u002Farticles",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e83c35d9"}}}}(null));
