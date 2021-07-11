8978938aec309c6f04a56c2883d1adb33ca5d230
rclement <rclement@users.noreply.github.com>
2021-07-11 13:42:03 +0000

    deploy: 8cdd5c14090931559da2e3814b043f46688ba163

--- _nuxt/static/1625652134/und/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1625652134",layout:"default",error:a,serverRendered:true,routePath:"\u002Fund",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"324b1d29"}}}}(null));
---
+++ _nuxt/static/1626010863/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1626010863",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"da4f464a"}}}}(null,"\u002F"));
