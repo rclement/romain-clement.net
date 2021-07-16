236917ea566cf9742a500cb16b6768882271e430
rclement <rclement@users.noreply.github.com>
2021-07-16 12:37:17 +0000

    deploy: 8b593d28d6e01e7e84a074a3b7e5b5bf7895b40f

--- _nuxt/static/1626010863/und/articles/cloudflare-setup-cdn-paas/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1626010863",layout:"default",error:a,serverRendered:true,routePath:"\u002Fund\u002Farticles\u002Fcloudflare-setup-cdn-paas",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"da4f464a"}}}}(null));
