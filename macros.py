f3319cc29d5cd39dcf5970ecf5df9da6394ae5b6
Romain Clement <contact@romain-clement.net>
2021-09-15 17:04:17 +0200

    Add some ci for python utility code

--- macros.py
+++ macros.py
@@ -1,16 +1,20 @@
-def readtime_minutes(content, wpm=200):
-    import readtime
+import readtime
 
+from babel.dates import format_date
+from mkdocs_macros.plugin import MacrosPlugin
+
+
+def readtime_minutes(content: str, wpm: int = 200) -> int:
     result = readtime.of_markdown(content, wpm=wpm)
-    return result.minutes
+    minutes: int = result.minutes
+    return minutes
 
 
-def localized_date(value, format="medium"):
-    from babel.dates import format_date
+def localized_date(date_str: str, format: str = "medium", locale: str = "en") -> str:
+    locale_date_str: str = format_date(date_str, format=format, locale=locale)
+    return locale_date_str
 
-    return format_date(value, format=format, locale="en")
 
-
-def define_env(env):
+def define_env(env: MacrosPlugin) -> None:
     env.filters["readtime"] = readtime_minutes
     env.filters["localized_date"] = localized_date
