b2c36d81cfdf031c2ccb235ce009d9d33b3a3da0
Romain Clement <contact@romain-clement.net>
2021-09-18 11:16:13 +0200

    Fix mkdocs config for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,11 +8,16 @@ from mkdocs.commands import build
 
 
 @pytest.fixture(scope="session", autouse=True)
-def mkdocs_build() -> Generator[config.Config, None, None]:
-    mkdocs_config = config.load_config()
-    mkdocs_config["site_dir"] = str((Path(__file__).parent / "site").absolute())
+def mkdocs_config() -> config.Config:
+    cfg = config.load_config()
+    cfg["site_dir"] = str((Path(__file__).parent / "site").absolute())
+    cfg["site_url"] = "http://localhost"
+    cfg["extra"]["forms"]["contact"]["base_url"] = "http://mailer.local"
+    return cfg
+
+
+@pytest.fixture(scope="session", autouse=True)
+def mkdocs_build(mkdocs_config: config.Config) -> Generator[None, None, None]:
     build.build(mkdocs_config)
-
-    yield mkdocs_config
-
+    yield
     rmtree(mkdocs_config["site_dir"])
--- tests/test_feeds.py
+++ tests/test_feeds.py
@@ -14,8 +14,8 @@ from mkdocs.config import Config
         Path("feed") / "articles" / "atom.xml",
     ),
 )
-def test_valid_rss_atom_feed(mkdocs_build: Config, feed_file: Path) -> None:
-    feed_path = Path(mkdocs_build["site_dir"]) / feed_file
+def test_valid_rss_atom_feed(mkdocs_config: Config, feed_file: Path) -> None:
+    feed_path = Path(mkdocs_config["site_dir"]) / feed_file
     feed = feedparser.parse(feed_path)
 
     channel_elements = ["title", "description", "link", "updated", "generator"]
@@ -28,10 +28,10 @@ def test_valid_rss_atom_feed(mkdocs_build: Config, feed_file: Path) -> None:
             assert el in entry
 
 
-def test_valid_json_feed(mkdocs_build: Config) -> None:
+def test_valid_json_feed(mkdocs_config: Config) -> None:
     json_feed_schema_path = Path(__file__).parent / "json-feed-schema-v1.1.json"
     json_feed_schema = json.loads(json_feed_schema_path.read_text())
-    json_feed_path = Path(mkdocs_build["site_dir"]) / "feed" / "articles" / "feed.json"
+    json_feed_path = Path(mkdocs_config["site_dir"]) / "feed" / "articles" / "feed.json"
     json_feed = json.loads(json_feed_path.read_text())
 
     jsonschema.validate(instance=json_feed, schema=json_feed_schema)
